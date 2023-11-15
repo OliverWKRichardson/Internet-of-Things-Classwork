df6137925a098ac4b0230de9375e689adc89d8e9
Oliver Richardson <or83@kent.ac.uk>
2023-11-15 12:20:53 +0000

    fix: temp sensor

--- Assignment2/Assignment2-Testing/Assignment2-Testing.ino
+++ Assignment2/Assignment2-Testing/Assignment2-Testing.ino
@@ -1,8 +1,8 @@
 #include <dummy.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
-#include <dht.h>
 #include <BH1750.h>
+#include <DHT.h>
 
 #define MQTT_BROKER "test.mosquitto.org"
 #define MQTT_PORT (1883)
@@ -10,12 +10,14 @@
 #define MQTT_PUBLIC_TOPIC "uok/iot/OR83/SmartFlowerPot"
 #define MQTT_SUBSCRIBE_TOPIC "uok/iot/OR83/SFPManualActivation"
 
-#define HUMIDITY_TEMP_DATAPIN 1
-dht DHT;
-BH1750 lightMeter(0x23);
+DHTNEW DHT(0);
+BH1750 lightMeter;
 
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
+
+String SSID;
+String WIFI_PWD;
 
 // print recieved string
 void printRecievedString(const char* topic, byte* payload, unsigned int length)
@@ -33,12 +35,16 @@ void setup() {
   Serial.begin(115200);
   // get wifi and password from serial line
   Serial.println("Enter Wifi Name:");
-  SSID = Serial.readString()
+  while(Serial.available() <= 0)
+  {}
+  SSID = Serial.readString();
   Serial.println("Enter Wifi Password:");
-  WIFI_PWD = Serial.readString()
-
+  while(Serial.available() <= 0)
+  {}
+  WIFI_PWD = Serial.readString();
   // connect to the wifi
-  Serial.print("Connecting to " SSID);
+  Serial.print("Connecting to ");
+  Serial.print(SSID);
   WiFi.begin(SSID, WIFI_PWD);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -62,27 +68,43 @@ void setup() {
   // subscribe to topic and set callback function to call when msg arrives
   client.subscribe(MQTT_SUBSCRIBE_TOPIC);
   client.setCallback(printRecievedString);
+  // set up light sensor
+  lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE);
+  lightMeter.configure(BH1750::ONE_TIME_HIGH_RES_MODE);
+  // set up temperture and humidity sensor
+  DHT.setWaitForReading(true);
+  DHT.setType(22);
 }
 
 void loop() {
   // MQTT loops
   client.loop();
   // main loop
-  Serial.begin(115200)
+  Serial.begin(115200);
   // temp and humidity
-  int readData = DHT.read22(HUMIDITY_TEMP_DATAPIN);
-  float temp = DHT.temperature;
-  float humid = DHT.humidity;
+  DHT.read();
+  float temp = DHT.getTemperature();
+  float humid = DHT.getHumidity();
   Serial.print("Temperture = ");
-  Serial.print(t);
+  Serial.flush();
+  Serial.print(temp);
+  Serial.flush();
   Serial.print("    Humidity = ");
-  Serial.print(h);
-  Serial.println("");
-  Serial.end();
+  Serial.flush();
+  Serial.print(humid);
+  Serial.flush();
+  Serial.print("    Sensor type: ");
+  Serial.flush();
+  Serial.println(DHT.getType());
+  Serial.flush();
   // light
-  lightmeter.begin();
+  while (!lightMeter.measurementReady(true)) 
+  {}
   float lux = lightMeter.readLightLevel();
-  Serial.print("Light: ");
-  Serial.print(lux);
-  delay(2000)
+  Serial.print("    Light = ");
+  Serial.flush();
+  Serial.println(lux);
+  Serial.flush();
+  Serial.end();
+  delay(2000);
 }
